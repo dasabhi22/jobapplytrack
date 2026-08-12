fea8775725f4ac1ec752469a6f3225ca8b6aa177
Abhiraj Das <abhirajdas2207001@gmail.com>
2026-08-12 11:16:42 +0530

    added protected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,26 @@
 import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/" 
+          element={
+            <Navigate to="/login" replace />
+          } 
+        />
         <Route path="/login" element = {<Login />} />
         <Route path="/register" element = {<Register />} />
+        <Route path="/dashboard" 
+          element = {
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   )
